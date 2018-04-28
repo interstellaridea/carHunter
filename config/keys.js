ec329991fbd6f55a22f3be311e12a20fdbbbb138
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-04-27 19:42:35 -0700

    Use keys based on env

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - what credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // were in production
+  module.exports = require('./prod');
+} else {
+  //return dev keys
+  module.exports = require('./dev');
+}
